e8d0677e3b9604192559c5d310ea9b6b67b70640
Cicero Galter <ehgalter@Ciceros-MacBook.local>
2021-02-19 17:27:28 -0300

    testing an useMemo hook using axios result filter

--- usememo/src/App.js
+++ usememo/src/App.js
@@ -1,14 +1,24 @@
 import { useMemo, useCallback, useState, useEffect } from 'react';
 import axios from 'axios';
 
+const filter = (users, query) => {
+  console.log('-----FILTER------');
+  return users.filter(user => user.name.toLowerCase().includes(query));
+}
+
+const UserList = ({ users, query }) => {
+  const filtered = useMemo(() => filter(users, query), [users, query]);
+
+  return filtered.map(user => <div key={user.id}>{user.name}</div>);
+}
+
 function App() {
   const [count, setCount] = useState(0);
-  const [query, setQuery] = useState();
+  const [query, setQuery] = useState('');
   const [users, setUsers] = useState([]);
 
   const getUsers = useCallback((async () => {
-    const { data } = await axios.get('https://jsonplaceholder.typicode.com/users/');
-
+    const { data } = await axios.get('https://jsonplaceholder.typicode.com/users/'); 
     setUsers(data);
   }), []);
 
@@ -24,6 +34,7 @@ function App() {
 
       <div>count: {count}</div>
       <button onClick={()  => setCount(prev => prev + 1)}>Increment</button>
+     <UserList users={users} query={query} />
     </div>
   );
 }
