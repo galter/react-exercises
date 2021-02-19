312f5dacfabeff1a4ba97a4a0c466971dc5296f5
Cicero Galter <ehgalter@Ciceros-MacBook.local>
2021-02-19 16:55:49 -0300

    testing an useCallback function

--- usememo/src/App.js
+++ usememo/src/App.js
@@ -1,7 +1,29 @@
+import { useMemo, useCallback, useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [query, setQuery] = useState();
+  const [users, setUsers] = useState([]);
+
+  const getUsers = useCallback((async () => {
+    const { data } = await axios.get('https://jsonplaceholder.typicode.com/users/');
+
+    setUsers(data);
+  }), []);
+
+  useEffect(() => {
+    getUsers()
+  }, [getUsers]);
+
   return (
     <div className="App">
+      <h1>useCallback vs useMemo</h1>
+
+      <input type="text" onChange={(e) => setQuery(e.target.value)} />
+
+      <div>count: {count}</div>
+      <button onClick={()  => setCount(prev => prev + 1)}>Increment</button>
     </div>
   );
 }
